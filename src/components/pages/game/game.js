af3ad92adaa0449b39df79442b890a0df875d847
DenisdS <denis_santos1989@hotmail.com>
2020-04-08 12:15:32 -0300

    Create function of attack and generate range of damage

--- src/components/pages/game/game.js
+++ src/components/pages/game/game.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Game = () => {
+
+  const attack = () => {
+    setRangeAtc(Math.floor(Math.random() * 101))
+  }
+
   return (
     <>
       <h1>Game</h1>
+
+      <button type="button" onClick={() => attack()}>Attack</button>
     </>
   )
 }
