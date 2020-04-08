e4d3df4707123f76adea9e88d30fece15fb8906e
DenisdS <denis_santos1989@hotmail.com>
2020-04-08 18:12:29 -0300

    Update life after player suffer attack

--- src/components/pages/game/game.js
+++ src/components/pages/game/game.js
@@ -1,38 +1,27 @@
 import React, { useState, useEffect } from 'react'
 
 const Game = () => {
-  const [rangeAttack, setRangeAtc] = useState(null)
-  const [rangeDefense, setRangeDf] = useState(null)
+  const [life, setLife] = useState (100)
 
-  const rangeAtc = () => {
-    return setRangeAtc(Math.floor(Math.random() * 101))
+  const rangeDamage = () => {
+    return (Math.floor(Math.random() * 101))
   }
 
-  const rangeDf = () => {
-    return setRangeDf(Math.floor(Math.random() * 101))
-  }
-
-  const attackP1 = () => {
-    rangeAtc()
-    rangeDf()
-  }
-
-  const attackP2 = () => {
-    rangeAtc()
-    rangeDf()
+  const attack = () => {
+    const damage = rangeDamage()
+    console.log(`Dano de ataque: ${damage}`)
+    return setLife(life - damage)
   }
 
   useEffect(() => {
-    console.log(`Ataque ${rangeAttack}`)
-    console.log(`Defesa ${rangeDefense}`)
-  });
+    console.log((`Vida atual: ${life}`))
+  }, [life])
 
   return (
     <>
       <h1>Game</h1>
 
-      <button type="button" onClick={() => attackP1()}>P1 Attack</button>
-      <button type="button" onClick={() => attackP2()}>P2 Attack</button>
+      <button type="button" onClick={() => attack()}>P1 Attack</button>
     </>
   )
 }
