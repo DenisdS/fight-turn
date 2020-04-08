5d671c71048d08bce3fac7dce87eda7c0e3cf475
DenisdS <denis_santos1989@hotmail.com>
2020-04-08 12:48:00 -0300

    Create range for defense of attack

--- src/components/pages/game/game.js
+++ src/components/pages/game/game.js
@@ -1,16 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Game = () => {
+  const [rangeAttack, setRangeAtc] = useState(null)
+  const [rangeDefense, setRangeDf] = useState(null)
 
-  const attack = () => {
-    setRangeAtc(Math.floor(Math.random() * 101))
+  const rangeAtc = () => {
+    return setRangeAtc(Math.floor(Math.random() * 101))
   }
+
+  const rangeDf = () => {
+    return setRangeDf(Math.floor(Math.random() * 101))
+  }
+
+  const attackP1 = () => {
+    rangeAtc()
+    rangeDf()
+  }
+
+  const attackP2 = () => {
+    rangeAtc()
+    rangeDf()
+  }
+
+  useEffect(() => {
+    console.log(`Ataque ${rangeAttack}`)
+    console.log(`Defesa ${rangeDefense}`)
+  });
 
   return (
     <>
       <h1>Game</h1>
 
-      <button type="button" onClick={() => attack()}>Attack</button>
+      <button type="button" onClick={() => attackP1()}>P1 Attack</button>
+      <button type="button" onClick={() => attackP2()}>P2 Attack</button>
     </>
   )
 }
